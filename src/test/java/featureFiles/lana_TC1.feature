409592884d8bf913e82d8e040ebd9a96c8050fec
ramamurthy <srinivasan.ramamurthy@gvl.de>
2019-10-22 18:31:21 +0200

    original master

---
+++ src/test/java/featureFiles/lana_TC1.feature
@@ -0,0 +1,17 @@
+Feature:Automate Lana labs UI Application
+  Description :
+  Scenario: Verify the filter functionality is working
+    Given enter the URL to launch the application
+    When verify that login is successful
+    And choose a existing log from alert
+    And select a country from filter attribute option
+    Then verify the filter is applied
+    And filter a specific attribute from pie chart
+    Then verify the case count is shown on the upper card
+    Then complete the test verification
+
+
+
+
+
+
---
+++ src/test/java/featureFiles/step_definitions/MyStepdefs.java
@@ -0,0 +1,24 @@
+package featureFiles.step_definitions;
+
+import cucumber.api.java8.En;
+
+public class MyStepdefs implements En {
+    public MyStepdefs() {
+        Given( "^enter the URL to launch the application$", () -> {
+        } );
+        When( "^verify that login is successful$", () -> {
+        } );
+        And( "^choose a existing log from alert$", () -> {
+        } );
+        And( "^select a country from filter attribute option$", () -> {
+        } );
+        Then( "^verify the filter is applied$", () -> {
+        } );
+        And( "^filter a specific attribute from pie chart$", () -> {
+        } );
+        Then( "^verify the case count is shown on the upper card$", () -> {
+        } );
+        Then( "^complete the test verification$", () -> {
+        } );
+    }
+}
